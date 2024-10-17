5776870a019504e65280d72057b42c305640bd73
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-10-17 20:43:26 +0530

    chore: move authMiddleware.ts from routes to middleware folder

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,19 @@
+import { MiddlewareHandler } from "hono"
+import { verify } from "hono/jwt"
+
+export const authMiddlware:MiddlewareHandler = async (c, next) => {
+    const authToken = c.req.header('Authorization')
+    if(!authToken) {
+        c.status(401)
+        return c.json({ error: "unauthorized" })
+    } 
+
+    const token = authToken.split(' ')[1]
+    const payload = await verify(token, c.env.JWT_SECRET)
+    if(!payload) {
+        c.status(401)
+        return c.json({ error: "unauthorized"})
+    }
+    c.set('userId', payload.id)
+    await next()
+}
