a3c5a52f07132d7e79cb723103f1f111df8cedd1
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-10-27 00:25:48 +0530

    refactor: move user signin route logic to a dedicated controller

---
+++ backend/src/controllers/userSigninController.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Context } from "hono";
+import { userSigninSchema } from "../types";
+import bcrypt from 'bcryptjs';
+import { sign } from "hono/jwt";
+
+export const userSigninController = async (c: Context) => {
+    try {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate())
+        
+        const body = await c.req.json()
+        
+        // validation using zod 
+        const response = userSigninSchema.safeParse(body)
+        if(!response.success) {
+            return c.json({message: "Invalid credentials"})
+        }
+        
+        // verify user 
+        const isExistingUser = await prisma.user.findFirst({
+            where: {
+              email: body.email
+            }
+        })
+        if(!isExistingUser) {
+            return c.json({message: 'Invalid credentials'})
+        }
+        
+        //verify password
+        const hashedPassword = isExistingUser.password
+        const plainTextPassword = body.password
+    
+        const isPasswordCorrect = await bcrypt.compare(plainTextPassword, hashedPassword)
+        if(isPasswordCorrect) {
+            const token = await sign({id: isExistingUser.id}, c.env.JWT_SECRET)
+            return c.json({token: token}, 200)
+        } else {
+            return c.json({message: "Invalid credentials"})
+        }
+    } catch(err) {
+        return c.json({error: 'Error while logging in', err}, 500)
+    }
+}
