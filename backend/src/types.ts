8994082778e4b1cc289f20a9847bb4205af19d60
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-10-27 02:20:46 +0530

    add type inference for prisma error

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -12,4 +12,12 @@ export const userSigninSchema = z.object({
     password: z.string(),
 })
 
-
+export interface PrismaClientError {
+    name: string;                   // Name of the error
+    code: string;                   // Error code (e.g., "P2002")
+    clientVersion: string;          // Version of the Prisma client
+    meta: {
+        modelName: string;          // Name of the model (e.g., "User")
+        target: string[];           // Array of fields causing the error (e.g., ["username"])
+    };
+}
