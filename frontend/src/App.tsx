36ebde96f3f89441786167ea88c25ddc4d08e6f7
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-11-02 04:14:04 +0530

    add routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter, Route,  Routes } from 'react-router-dom'
+import { Home } from "./pages/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        hello there
-      </div>
+      <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Home />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
