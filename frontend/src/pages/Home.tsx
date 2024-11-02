8eb8feb41b358d21fae0020326097146ec43445c
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-11-03 02:44:24 +0530

    style: apply minor styling changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,25 @@
-import Navbar from "../components/Navbar"
+import { Button } from "../components/Button"
+import { Navbar } from "../components/Navbar"
 
 export const Home = () => {
-  return (
-    <>
-        <Navbar />
+  const handleSignup = () => {
+    // handle signup
+  }
 
-        <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="w-10/12">
-          <h1 className="font-mono">
-            <span className="font-bold">BlogNest</span> is a welcoming platform for writers to share thoughts and stories in a supportive space. Designed for simplicity, it allows users to craft and customize posts easily, fostering creativity and connection. Whether you’re a seasoned blogger or just starting, BlogNest is the perfect place to build your online presence and connect with a like-minded community.
-          </h1>
-        </div>
-    </div>
-    </>
-    
+  return (
+      <>
+          <Navbar />
+          
+          <div className="w-full h-[30rem] flex flex-col justify-center items-center">
+              <div className="max-w-7xl flex justify-center items-center w-full px-6">
+                  <h1 className="font-mono text-xl text-justify">
+                      <span className="font-bold">BlogNest</span> is a welcoming platform for writers to share thoughts and stories in a supportive space. Designed for simplicity, it allows users to craft and customize posts easily, fostering creativity and connection. Whether you're a seasoned blogger or just starting, BlogNest is the perfect place to build your online presence and connect with a like-minded community.
+                  </h1>
+              </div>
+              <div className="my-6">
+                <Button label={"Get Started"} onClick={handleSignup} solid={true} />
+              </div>
+          </div>
+      </>
   )
 }
