eb19a2be9591d1ee5ca35fc71be9c6c5853cbd36
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-11-19 01:37:24 +0530

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
 import userRouter from './routes/user'
 import blogRouter from './routes/blog'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
+
+app.use('/*', cors())
 app.get('/', (c) => {
   return c.json({msg: 'Hello Hono!'})
 })
