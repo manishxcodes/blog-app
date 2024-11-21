40149c8dcf76ac8cc0579e32351bd2fbb54cfaef
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-11-22 01:21:29 +0530

    add autoComplete in InputBoxProps

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -3,17 +3,18 @@ interface InputBoxProps {
     label: string;
     name: string;
     value: string;
+    autoComplete?: string;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export const InputBox: React.FC<InputBoxProps> = ({ placeholder, label, name, value, onChange }) => {
+export const InputBox: React.FC<InputBoxProps> = ({ placeholder, label, name, value, autoComplete, onChange }) => {
   return (
     <div className="pb-4">
         <div className="font-mono font-semibold text-left text-md pb-1">
             {label}
         </div>
         <div>
-            <input type="text" placeholder={placeholder} name={name} value={value} onChange={onChange} 
+            <input type="text" placeholder={placeholder} name={name} value={value} autoComplete={autoComplete} onChange={onChange} 
             className="text-sm font-mono text-slate-400 border border-slate-300 rounded-md w-full px-2 py-1"
             />
         </div>
