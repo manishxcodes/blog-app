3c2a12730c4ae32dd2e1434733e9f754cdc59987
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-11-28 02:24:45 +0530

    add app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route,  Routes } from 'react-router-dom'
 import { Home } from "./pages/Home"
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Main } from './pages/Main'
+import { Bookmarks } from './pages/Bookmarks'
+import { MyPosts } from './pages/MyPosts'
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
           <Routes>
-          <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/blogs' element={<Main />} />
+            <Route path='/bookmarks' element={<Bookmarks />} />
+            <Route path='/myposts' element={<MyPosts />} />
           </Routes>
       </BrowserRouter>
     </>
