2ab46603712f647dd6bdff96596a3d4d39522397
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-12-02 01:59:22 +0530

    fix: fetching blog issue

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,33 +1,39 @@
+import { useEffect } from "react";
 import { BlogCard } from "../components/BlogCard"
+import { ErrorMessage } from "../components/ErrorMessage";
+import { Loader } from "../components/Loader";
 import { Navbar } from "../components/Navbar"
 import { useBlogs } from "../hooks/useBlogs"
 
 export const Main = () => {
-  const { blogs, loading, error } = useBlogs();
+  const { blogs, loading, error, fetchBlogs } = useBlogs();
 
+
+  useEffect(() => {
+    if(blogs.length == 0) {
+      fetchBlogs()
+    }
+  }, [ blogs.length, fetchBlogs])
+  
+ 
   if(loading) {
     return (
-      <div className="h-screen flex justify-center items-center">
-          <h3 className="font-mono font-bold text-xl">Loading</h3>
-      </div>
+      <Loader />
     )
   }
 
   if(error) {
     return (
-      <div className="h-screen flex justify-center items-center">
-          <h3 className="font-mono font-bold text-xl">Error: {error}</h3>
-      </div>
+      <ErrorMessage error={error} />
     )
   }
-  console.log("blogs:", blogs)
   
   return (
     <div>
         <Navbar />
         <section className="flex justify-center items-center">
-          <div className=" h-screen max-w-7xl w-full px-6">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-2">
+          <div className=" h-screen max-w-6xl w-full px-6">
+            <div className="grid grid-cols-1 gap-y-3">
               { 
                 blogs.map((blog) => (
                   <BlogCard key={blog.id} 
