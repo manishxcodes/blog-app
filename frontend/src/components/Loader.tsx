4bd7e866f4d58e0172d3b876ef3150d05b6f9e51
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-12-02 00:03:54 +0530

    add Loader component

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+        <h3 className="font-mono font-bold text-xl">Loading</h3>
+    </div>
+  )
+}
