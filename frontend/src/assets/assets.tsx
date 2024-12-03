0c658868f0476ab9ce2215b1fc4c095329e029c6
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-12-03 19:54:47 +0530

    add assests

--- frontend/src/assets/assets.tsx
+++ frontend/src/assets/assets.tsx
@@ -1,4 +1,4 @@
-export const githubLogo = <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+export const githubLogo = <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
 </svg>
@@ -9,20 +9,28 @@ export const crossIcon = <svg width="35px" height="35px" viewBox="0 0 24 24" fil
 
 export const hamburgerMenu = <svg width="35px" height="35px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" >
     <title>Menu</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Menu">
-            <rect id="Rectangle" fill-rule="nonzero" x="0" y="0" width="24" height="24">
+            <rect id="Rectangle" fillRule="nonzero" x="0" y="0" width="24" height="24">
 
 </rect>
-            <line x1="5" y1="7" x2="19" y2="7" id="Path" stroke="#0C0310" stroke-width="2" stroke-linecap="round">
+            <line x1="5" y1="7" x2="19" y2="7" id="Path" stroke="#0C0310" strokeWidth="2" strokeLinecap="round">
 
 </line>
-            <line x1="5" y1="17" x2="19" y2="17" id="Path" stroke="#0C0310" stroke-width="2" stroke-linecap="round">
+            <line x1="5" y1="17" x2="19" y2="17" id="Path" stroke="#0C0310" strokeWidth="2" strokeLinecap="round">
 
 </line>
-            <line x1="5" y1="12" x2="19" y2="12" id="Path" stroke="#0C0310" stroke-width="2" stroke-linecap="round">
+            <line x1="5" y1="12" x2="19" y2="12" id="Path" stroke="#0C0310" strokeWidth="2" strokeLinecap="round">
 
 </line>
         </g>
     </g>
+</svg>
+
+export const bookmarkLogoLight = <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="33" height="33" viewBox="0 0 32 32">
+<path d="M 7 5 L 7 28 L 8.59375 26.8125 L 16 21.25 L 23.40625 26.8125 L 25 28 L 25 5 Z M 9 7 L 23 7 L 23 24 L 16.59375 19.1875 L 16 18.75 L 15.40625 19.1875 L 9 24 Z"></path>
+</svg>
+
+export const bookmarkLogoNight = <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24">
+<path d="M 4 2 L 4 22 L 12 19 L 20 22 L 20 2 L 6 2 L 4 2 z"></path>
 </svg>
