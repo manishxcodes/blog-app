b3947a0fed5378557d6ed1a77a9a260d87288d81
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-12-18 02:41:44 +0530

    add blogpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,20 @@ import { Signin } from './pages/Signin'
 import { Main } from './pages/Main'
 import { Bookmarks } from './pages/Bookmarks'
 import { MyPosts } from './pages/MyPosts'
-
+import { BlogPage } from './pages/BlogPage'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <>
+      <ToastContainer position="top-right" autoClose={3000} />
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/signin' element={<Signin />} />
             <Route path='/blogs' element={<Main />} />
+            <Route path='/blog/:id' element={<BlogPage />} />
             <Route path='/bookmarks' element={<Bookmarks />} />
             <Route path='/myposts' element={<MyPosts />} />
           </Routes>
